ac336b92917fb482d015c9a216a4c8ce5fbfcec6
Max <Max@Leos-MBP.home>
2015-03-18 02:21:16 +0000

    Cleaning up

--- Battleship Game/AI.m
+++ Battleship Game/AI.m
@@ -150,21 +150,20 @@
     }
     NSMutableArray *chosencols = [[NSMutableArray alloc] init];
     for(int i=0; i<colcounts.count; i++)
-        {
-            if([colcounts[i] integerValue]==largestcol)
-                [chosencols addObject:[NSNumber numberWithInt:i]];
-        }
+    {
+        if([colcounts[i] integerValue]==largestcol)
+            [chosencols addObject:[NSNumber numberWithInt:i]];
+    }
     randomIndex = arc4random_uniform((int)[chosencols count]);
     int c1 = (int)[[chosencols objectAtIndex:randomIndex] integerValue];
     int c = (int)[[[Rows objectAtIndex:r] objectAtIndex:c1] integerValue];
     [[Rows objectAtIndex:r] removeObjectAtIndex:c1];
-    //[[Rows objectAtIndex:r] remo];
     NSMutableArray *temp = [[NSMutableArray alloc] init];
     temp = [self CheckEligibility:c :r];
     if (Map [(r * 10) + c] > 1)
     {
-        [temp replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];//=false;
-        [temp replaceObjectAtIndex:1 withObject:[NSNumber numberWithBool:false]];//=false;
+        [temp replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];
+        [temp replaceObjectAtIndex:1 withObject:[NSNumber numberWithBool:false]];
     }
     if ([[temp objectAtIndex:0] boolValue]==false&&[[temp objectAtIndex:1] boolValue]==false)
     {
@@ -181,12 +180,18 @@
 -(NSMutableArray*) Destroy
 {
     NSMutableArray *results = [[NSMutableArray alloc] init];
-    int c = HitPoint%10; int r = HitPoint/10;
+    int c = HitPoint%10;
+    int r = HitPoint/10;
     NSMutableArray *eligible = [self CheckEligibility:c :r];
-    bool h = [[eligible objectAtIndex:0] boolValue]; bool v = [[eligible objectAtIndex:1] boolValue];
-    int tempcol = c; int temprow = r;
+    bool h = [[eligible objectAtIndex:0] boolValue];
+    bool v = [[eligible objectAtIndex:1] boolValue];
+    int tempcol = c;
+    int temprow = r;
     
-    int up = ((r+1)*10)+c; int down = ((r-1)*10)+c; int left = (r*10)+c-1; int right = (r*10)+c+1;
+    int up = ((r+1)*10)+c;
+    int down = ((r-1)*10)+c;
+    int left = (r*10)+c-1;
+    int right = (r*10)+c+1;
     
     NSMutableArray *u1 = [[NSMutableArray alloc] init];
     [u1 addObject:[NSNumber numberWithBool:true]];
@@ -204,10 +209,10 @@
     [r1 addObject:[NSNumber numberWithBool:true]];
     [r1 addObject:[NSNumber numberWithBool:false]];
     
-    if(r==9)[u1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];//= false;
-    if(r==0)[d1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];//= false;
-    if(c==0)[l1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];//= false;
-    if(c==9)[r1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];//= false;
+    if(r==9)[u1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];
+    if(r==0)[d1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];
+    if(c==0)[l1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];
+    if(c==9)[r1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];
     
     if([[u1 objectAtIndex:0]boolValue]&&Map[up]==HIT)
         [u1 replaceObjectAtIndex:1 withObject:[NSNumber numberWithBool:true]];
@@ -218,20 +223,26 @@
     if([[r1 objectAtIndex:0]boolValue]&&Map[right]==HIT)
         [r1 replaceObjectAtIndex:1 withObject:[NSNumber numberWithBool:true]];
     
-    int nextup=-1;int nextdown=-1;int nextleft=-1;int nextright=-1;
+    int nextup=-1;
+    int nextdown=-1;
+    int nextleft=-1;
+    int nextright=-1;
     
     while (temprow<9 && Map[(temprow*10)+tempcol]==HIT) {temprow++;}
     if (Map [(temprow * 10) + tempcol] == WATER && v) {nextup=temprow;}
     else{[u1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];}
-    tempcol = c; temprow = r;
+    tempcol = c;
+    temprow = r;
     while (temprow>0 && Map[(temprow*10)+tempcol]==HIT) {temprow--;}
     if (Map [(temprow * 10) + tempcol] == WATER && v) {nextdown = temprow;}
     else{[d1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];}
-    tempcol = c; temprow = r;
+    tempcol = c;
+    temprow = r;
     while (tempcol>0 && Map[(temprow*10)+tempcol]==HIT) {tempcol--;}
     if (Map [(temprow * 10) + tempcol] == WATER && h) {nextleft=tempcol;}
     else{[l1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];}
-    tempcol = c; temprow = r;
+    tempcol = c;
+    temprow = r;
     while (tempcol<9 && Map[(temprow*10)+tempcol]==HIT) {tempcol++;}
     if (Map [(temprow * 10) + tempcol] == WATER && h) {nextright=tempcol;}
     else{[r1 replaceObjectAtIndex:0 withObject:[NSNumber numberWithBool:false]];}
